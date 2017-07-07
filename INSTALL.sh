5d5364c7ecaeb87c231be8edbdfb1e4e42840810
J Forde <jzf2101@columbia.edu>
2017-07-07 11:08:55 -0400

    added back bqplot to demo @jasongrout

--- INSTALL.sh
+++ INSTALL.sh
@@ -4,6 +4,11 @@ export DEMO_ENV_NAME=jupyterlab-demo
 conda env create -f environment.yml
 source activate $DEMO_ENV_NAME
 
+# Currently need prereleases of widgets
+# and bqplot
+pip install bqplot --pre
+pip install ipywidgets --pre
+
 # Install the environment kernel
 ipython kernel install --name $DEMO_ENV_NAME --display-name $DEMO_ENV_NAME --sys-prefix
 
@@ -13,6 +18,10 @@ jupyter lab clean && jupyter lab build
 
 # Install ipywidgets
 jupyter labextension install @jupyterlab/nbwidgets@0.21
+
+#Install bqplot
+jupyter labextension install @jupyter-widgets/jupyterlab-manager
+jupyter labextension install bqplot-jupyterlab
 
 # To remove the environment
 # source deactivate $DEMO_ENV_NAME
