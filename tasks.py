fd0766e494d0725eeca7ab22a86af7c899d1530e
Jason Grout <jgrout6@bloomberg.net>
2017-07-14 02:42:35 -0500

    Update the environment for jlab 0.25

--- tasks.py
+++ tasks.py
@@ -9,17 +9,20 @@ demofolder = 'demofiles'
 def environment(ctx, clean=False, env_name=env_name):
 	if clean:
 		print('deleting environment')
-		ctx.run('source deactivate; conda remove -n {} --all'.format(env_name))
+		ctx.run('source deactivate; conda remove -n {0!s} --all'.format(env_name))
 	# Create a new environment
-	print('creating environment')
-	ctx.run('conda env create -f environment.yml -n {0!s};\
-		source activate {0!s};\
-		ipython kernel install --name {0!s} --display-name {0!s} --sys-prefix;\
-		jupyter labextension install @jupyterlab/google-drive@0.3.1;\
-		jupyter lab clean && jupyter lab build;\
-		jupyter labextension install @jupyterlab/nbwidgets@0.21;\
-		jupyter labextension install @jupyter-widgets/jupyterlab-manager;\
-		jupyter labextension install bqplot-jupyterlab'.format(env_name), pty=True)
+	print('creating environment {0!s}'.format(env_name))
+	ctx.run("""
+	    conda env create -f environment.yml -n {0!s};
+		source activate {0!s};
+		ipython kernel install --name {0!s} --display-name {0!s} --sys-prefix;
+		jupyter labextension install @jupyterlab/google-drive@0.4.0;
+		jupyter labextension install @jupyterlab/geojson-extension@0.8.0;
+		jupyter labextension install @jupyterlab/json-extension@0.8.0;
+		jupyter labextension install @jupyter-widgets/jupyterlab-manager@0.24.1;
+		jupyter labextension install bqplot-jupyterlab@0.4.0;
+		jupyter lab clean && jupyter lab build;
+		""".format(env_name), pty=True)
 
 @task
 def demofiles(ctx, clean=False, demofolder=demofolder):
@@ -31,13 +34,15 @@ def demofiles(ctx, clean=False, demofolder=demofolder):
 	old_pwd = os.getcwd()
 	os.chdir(demofolder)
 	#list of repos used in demo
-	print('cloning repos into demofolder')
-	reponames = ['jakevdp/PythonDataScienceHandbook',
-	'swissnexSF/Urban-Data-Challenge',
-	'altair-viz/altair',
-	'QuantEcon/QuantEcon.notebooks',
-	'theandygross/TCGA',
-	'aymericdamien/TensorFlow-Examples']
+	print('cloning repos into demo folder {}'.format(demofolder))
+	reponames = [
+		'jakevdp/PythonDataScienceHandbook',
+		'swissnexSF/Urban-Data-Challenge',
+		'altair-viz/altair',
+		'QuantEcon/QuantEcon.notebooks',
+		'theandygross/TCGA',
+		'aymericdamien/TensorFlow-Examples'
+	]
 	for repo in reponames:
 		ctx.run('git clone https://github.com/{}.git'.format(repo))
 		assert os.path.isdir(repo.split('/')[1]), '{} failed download'.format(repo)
