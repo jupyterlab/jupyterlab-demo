3d50cb29addb335228d1086fd189ba34ab28d7a1
Steven Silvester <steven.silvester@ieee.org>
2017-08-09 13:01:41 -0500

    Try the global config

--- tasks.py
+++ tasks.py
@@ -1,5 +1,5 @@
 from __future__ import print_function
-from invoke import task, Collection
+from invoke import task, Config
 import os
 import yaml
 import shutil
@@ -183,8 +183,7 @@ def talk(ctx, talk_name, clean=False):
 
 
 # Configure cross-platform settings.
-ns = Collection(environment, demofiles, clean, scipy2017)
-ns.configure({
+Config.from_data({
 	'run': {
 		'shell': which('bash') if os.name != 'nt' else which('cmd'),
 		'pty': False if os.name == 'nt' else True
