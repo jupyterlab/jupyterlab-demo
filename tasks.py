6fcb70da7d65b0f6ee128b300b417f45a09e8a08
Ian Rose <ian.r.rose@gmail.com>
2018-07-30 08:38:08 -0700

    Update to JupyterLab 0.33

--- tasks.py
+++ tasks.py
@@ -48,12 +48,11 @@ def build(ctx, env_name=env_name, kernel=True):
     '''
     ctx.run("""
         {0!s} activate {1!s} &&
-        jupyter labextension install @jupyterlab/fasta-extension@0.16 --no-build &&
-        jupyter labextension install @jupyterlab/geojson-extension@0.16 --no-build &&
-        jupyter labextension install @jupyterlab/google-drive@0.12 --no-build &&
-        jupyter labextension install @jupyterlab/plotly-extension@0.16 --no-build &&
-        jupyter labextension install @jupyter-widgets/jupyterlab-manager@0.35 --no-build &&
-        jupyter labextension install bqplot@0.3.6 --no-build &&
+        jupyter labextension install @jupyterlab/fasta-extension@0.17 --no-build &&
+        jupyter labextension install @jupyterlab/geojson-extension@0.17 --no-build &&
+        jupyter labextension install @jupyterlab/plotly-extension@0.17 --no-build &&
+        jupyter labextension install @jupyter-widgets/jupyterlab-manager@0.36 --no-build &&
+        jupyter labextension install bqplot@0.4.0 --no-build &&
         jupyter lab clean && jupyter lab build
         """.format(source, env_name).strip().replace('\n', ''))
     if kernel:
